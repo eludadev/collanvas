bca16c4677e727671910de1122c408787a5e495f
Eluda <111eluda111@gmail.com>
2022-08-28 17:11:55 +0100

    Integrate dotenv

--- server/index.mjs
+++ server/index.mjs
@@ -1,3 +1,4 @@
+import 'dotenv/config'
 import { createServer } from 'http'
 import { parse } from 'url'
 import { Server } from 'socket.io'
--- server/redis.mjs
+++ server/redis.mjs
@@ -2,7 +2,7 @@ import { createClient } from 'redis'
 
 const url =
   process.env.NODE_ENV === 'development'
-    ? `redis://localhost:6379`
+    ? 'redis://localhost:6379'
     : process.env.REDIS_URL
 export const connection = createClient({ url })
 await connection.connect()
