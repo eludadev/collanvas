7b508e2d3f78ac6c3856a2bc3f19266464a0dbf3
Eluda <111eluda111@gmail.com>
2022-08-28 16:05:43 +0100

    Use redis URL from env in production mode

--- server/redis.mjs
+++ server/redis.mjs
@@ -1,6 +1,9 @@
 import { createClient } from 'redis'
 
-const url = `redis://localhost:6379`
+const url =
+  process.env.NODE_ENV === 'development'
+    ? `redis://localhost:6379`
+    : process.env.REDIS_URL
 export const connection = createClient({ url })
 await connection.connect()
 
